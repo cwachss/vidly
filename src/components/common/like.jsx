625d26a1929fb26415fd12936c69e51701b6da0e
Chani Wachsstock <chaniwachsstock@gmail.com>
2022-08-24 15:18:39 +0300

    add like button

---
+++ src/components/common/like.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const Like = props => {
+    let style = 'regular';
+    if(props.liked) style = 'solid';
+    let classes = `fa-${style} fa-heart`;
+    return (<i onClick={props.onClick} style={{cursor: 'pointer'}} className={classes}></i>);
+}
+ 
+export default Like;
